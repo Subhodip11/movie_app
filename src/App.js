a622b1f5f160b742ed009c38e49350ef9036f5c8
Subhodip11 <1941012209.subhodip@gmail.com>
2022-02-22 21:51:11 +0530

    made changes

--- src/App.js
+++ src/App.js
@@ -1,53 +1,109 @@
-import React,{useState} from 'react'
-import MovieCardFullDetails from './Components/MovieCardFullDetails';
-import MovieSection from './Components/MovieSection'
-import NavBar from './Components/NavBar'
-import { BrowserRouter as Router,Switch,Route } from 'react-router-dom';
-import Progress from '@ramonak/react-progress-bar';
+import React, { useState } from "react";
+import MovieCardFullDetails from "./Components/MovieCardFullDetails";
+import MovieSection from "./Components/MovieSection";
+import NavBar from "./Components/NavBar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Progress from "@ramonak/react-progress-bar";
 
 const App = () => {
-  const apiKey=process.env.REACT_APP_MOVIE_API_KEY;
-  const [searchValue, setsearchValue] = useState('');
+  const apiKey = process.env.REACT_APP_MOVIE_API_KEY;
+  const [searchValue, setsearchValue] = useState("");
+  const [firstVisit, setFirstVisit] = useState(true);
   const [fullDetails, setfullDetails] = useState([]);
   const [progress, setprogress] = useState(0);
   const [showProgress, setshowProgress] = useState(false);
-  const closeContainer=()=>{
+  const closeContainer = () => {
     setfullDetails([]);
-  }
-  document.title='Movies Section- MFUD'
-    return ( 
+  };
+  document.title = "Movies Section- MFUD";
+  return (
     <div>
-    <Router>
-     {showProgress && <Progress 
-       completed={progress}
-       borderRadius='0'
-       height='0.2rem'
-       isLabelVisible={false}
-       bgColor='red'
-      />}
-      <NavBar searchValue={searchValue} setsearchValue={setsearchValue} />
-      <Switch>
-      <Route key='/' exact path='/'>
-     {fullDetails.length===0?"":<MovieCardFullDetails fullDetails={fullDetails} closeContainer={closeContainer}/>}
-      <MovieSection apiKey={apiKey} type={'movies'} searchedValue={searchValue} fullDetails={fullDetails} setfullDetails={setfullDetails} progress={setprogress} showProgress={setshowProgress}/>
-      
-      </Route>
-      
-      <Route key='series' exact path='/series'>
-     {fullDetails.length===0?"":<MovieCardFullDetails fullDetails={fullDetails} closeContainer={closeContainer}/>}
-      <MovieSection apiKey={apiKey} type={'series'} searchedValue={searchValue} fullDetails={fullDetails} setfullDetails={setfullDetails} progress={setprogress} showProgress={setshowProgress}/>
-      
-      </Route>
-      
-      <Route key='episodes' exact path='/episodes'>
-     {fullDetails.length===0?"":<MovieCardFullDetails fullDetails={fullDetails} closeContainer={closeContainer}/>}
-      <MovieSection apiKey={apiKey} type={'episodes'} searchedValue={searchValue} fullDetails={fullDetails} setfullDetails={setfullDetails} progress={setprogress} showProgress={setshowProgress}/>
-     
-      </Route>
-      </Switch>
-     </Router>
-    </div>
-    )
-}
+      <Router>
+        {showProgress && (
+          <Progress
+            completed={progress}
+            borderRadius="0"
+            height="0.2rem"
+            isLabelVisible={false}
+            bgColor="red"
+          />
+        )}
+        <NavBar searchValue={searchValue} setsearchValue={setsearchValue} />
 
-export default App
+        <MovieSection
+          apiKey={apiKey}
+          type={"movies"}
+          searchedValue={searchValue.length === 0 ? "avengers" : searchValue}
+          fullDetails={fullDetails}
+          setfullDetails={setfullDetails}
+          progress={setprogress}
+          showProgress={setshowProgress}
+        />
+
+        <Switch>
+          <Route key="/" exact path="/">
+            {fullDetails.length === 0 ? (
+              ""
+            ) : (
+              <MovieCardFullDetails
+                fullDetails={fullDetails}
+                closeContainer={closeContainer}
+              />
+            )}
+            <MovieSection
+              apiKey={apiKey}
+              type={"movies"}
+              searchedValue={searchValue}
+              fullDetails={fullDetails}
+              setfullDetails={setfullDetails}
+              progress={setprogress}
+              showProgress={setshowProgress}
+            />
+          </Route>
+
+          <Route key="series" exact path="/series">
+            {fullDetails.length === 0 ? (
+              ""
+            ) : (
+              <MovieCardFullDetails
+                fullDetails={fullDetails}
+                closeContainer={closeContainer}
+              />
+            )}
+            <MovieSection
+              apiKey={apiKey}
+              type={"series"}
+              searchedValue={searchValue}
+              fullDetails={fullDetails}
+              setfullDetails={setfullDetails}
+              progress={setprogress}
+              showProgress={setshowProgress}
+            />
+          </Route>
+
+          <Route key="episodes" exact path="/episodes">
+            {fullDetails.length === 0 ? (
+              ""
+            ) : (
+              <MovieCardFullDetails
+                fullDetails={fullDetails}
+                closeContainer={closeContainer}
+              />
+            )}
+            <MovieSection
+              apiKey={apiKey}
+              type={"episodes"}
+              searchedValue={searchValue}
+              fullDetails={fullDetails}
+              setfullDetails={setfullDetails}
+              progress={setprogress}
+              showProgress={setshowProgress}
+            />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+};
+
+export default App;
--- src/Components/MovieSection.jsx
+++ src/Components/MovieSection.jsx
@@ -1,46 +1,63 @@
-import React,{useState,useEffect} from 'react'
-import MovieCard from './MovieCard';
+import React, { useState, useEffect } from "react";
+import MovieCard from "./MovieCard";
 
 const MovieSection = (props) => {
-     const [MovieInfo, setMovieInfo] = useState([]);
-     if(props.type==='movies')
-     document.title=`Movies Section- MFUD`
-     if(props.type==='series')
-     document.title=`Series Section- MFUD`
-     if(props.type==='episodes')
-     document.title=`Episodes Section- MFUD`
-     
-     const apiCall=async ()=>{
-         props.showProgress(true);
-         props.progress(10);
-         const url=props.type==='movies'?`https://www.omdbapi.com/?apikey=${props.apiKey}&s=${props.searchedValue}`:`https://www.omdbapi.com/?apikey=c1b97b66&type=${props.type}&s=${props.searchedValue}`
-         props.progress(30);
-         let data=await fetch(url);
-         props.progress(50);
-         let response=await data.json();
-         let movieData=response['Search']
-         console.log(movieData,url);
-         setMovieInfo(movieData);
-         props.progress(100);
-         setTimeout(() => {
-             props.showProgress(false)
-         }, 2000);
-     }
-    
-     useEffect(() => {
-         apiCall();
-     }, [props.searchedValue])
-    return (
-        <div className="container mx-auto">
-        <div className='row'>
-           {MovieInfo?MovieInfo.map((element,index)=>{
-               return <div className="col-md-4" key={index}> 
-               <MovieCard   Title={element.Title} Year={element.Year} Poster={element.Poster} imdbID={element.imdbID} fullDetails={props.fullDetails} setfullDetails={props.setfullDetails} type={props.type}/>
-               </div>
-           }):(props.searchedValue.length===0?<h4>Searched {props.type} are displayed here</h4>:<h4>No match Found</h4>)}   
-        </div>
-        </div>
-    )
-}
+  const [MovieInfo, setMovieInfo] = useState([]);
+  if (props.type === "movies") document.title = `Movies Section- MFUD`;
+  if (props.type === "series") document.title = `Series Section- MFUD`;
+  if (props.type === "episodes") document.title = `Episodes Section- MFUD`;
 
-export default MovieSection
+  const apiCall = async () => {
+    props.showProgress(true);
+    props.progress(10);
+    const url =
+      props.type === "movies"
+        ? `https://www.omdbapi.com/?apikey=${props.apiKey}&s=${props.searchedValue}`
+        : `https://www.omdbapi.com/?apikey=c1b97b66&type=${props.type}&s=${props.searchedValue}`;
+    props.progress(30);
+    let data = await fetch(url);
+    props.progress(50);
+    let response = await data.json();
+    let movieData = response["Search"];
+    console.log(movieData, url);
+    setMovieInfo(movieData);
+    props.progress(100);
+
+    setTimeout(() => {
+      props.showProgress(false);
+    }, 2000);
+  };
+
+  useEffect(() => {
+    apiCall();
+  }, [props.searchedValue]);
+  return (
+    <div className="container mx-auto">
+      <div className="row">
+        {MovieInfo ? (
+          MovieInfo.map((element, index) => {
+            return (
+              <div className="col-md-4" key={index}>
+                <MovieCard
+                  Title={element.Title}
+                  Year={element.Year}
+                  Poster={element.Poster}
+                  imdbID={element.imdbID}
+                  fullDetails={props.fullDetails}
+                  setfullDetails={props.setfullDetails}
+                  type={props.type}
+                />
+              </div>
+            );
+          })
+        ) : props.searchedValue.length === 0 ? (
+          <h4>Searched {props.type} are displayed here</h4>
+        ) : (
+          <h4>No match Found</h4>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default MovieSection;
